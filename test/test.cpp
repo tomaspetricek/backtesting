675868ccd30ed4998b189c9a1ffa04ac9fdfa725
TomasPetricek <45512445+tomaspetricek@users.noreply.github.com>
2022-09-27 10:24:21 +0200

    Enable unit testing

---
+++ test/test.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Tomáš Petříček on 27.09.2022.
+//
+
+#define BOOST_TEST_MAIN
+#include "trading/wallet.hpp"
+
+BOOST_AUTO_TEST_CASE(example)
+{
+    BOOST_TEST(1==1);
+}
---
+++ test/trading/wallet.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Tomáš Petříček on 27.09.2022.
+//
+
+#ifndef BACKTESTING_TEST_WALLET_HPP
+#define BACKTESTING_TEST_WALLET_HPP
+
+#include <boost/test/unit_test.hpp>
+#include <trading/wallet.hpp>
+
+BOOST_AUTO_TEST_SUITE(wallet)
+
+BOOST_AUTO_TEST_SUITE_END()
+
+#endif //BACKTESTING_TEST_WALLET_HPP
