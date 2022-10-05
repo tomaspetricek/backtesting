f9d74fcd07feeb7f48c463093dfab2b18db44815
TomasPetricek <45512445+tomaspetricek@users.noreply.github.com>
2022-10-05 22:19:31 +0200

    Add position tests

--- test/trading_view.hpp
+++ test/trading_view.hpp
@@ -28,6 +28,8 @@
 #include <trading/trade_manager.hpp>
 #include <trading/trader.hpp>
 
+#include <trading/binance/futures/position.hpp>
+
 using namespace trading;
 using namespace io;
 using namespace bazooka;
@@ -266,7 +268,7 @@ BOOST_AUTO_TEST_SUITE(trading_view)
         };
 
         csv::reader<candle, long, double, double, double, double, double, double, double, double, double, double, double, double, double>
-                chart_reader{chart_data_csv, ';', chart_data_deserializer};
+                candle_reader{chart_data_csv, ';', chart_data_deserializer};
 
         std::filesystem::path trades_csv("../../data/in/spot-sma-list-of-trades.csv");
 
@@ -283,7 +285,7 @@ BOOST_AUTO_TEST_SUITE(trading_view)
                 trades_csv, ',', trades_deserializer};
 
         // read data
-        auto candles = chart_reader();
+        auto candles = candle_reader();
         auto trade_infos = trades_reader();
 
         // reverse trades
@@ -318,6 +320,63 @@ BOOST_AUTO_TEST_SUITE(trading_view)
                 }
             }
         }
+    }
+
+    BOOST_AUTO_TEST_CASE(no_leverage_test)
+    {
+        std::size_t leverage{1};
+        binance::futures::long_position pos{trade{amount_t{1159.0478454228034}, price_t{9'414.21}, ptime()}, leverage};
+        pos.add_close(trade{pos.size(), price_t{9'693.60}, ptime()});
+        BOOST_REQUIRE_CLOSE(value_of(pos.total_realized_profit<amount_t>()), 34.40, 0.01);
+        BOOST_REQUIRE_CLOSE(value_of(pos.total_realized_profit<percent_t>())*100, 2.97, 0.1);
+
+        price_t open{6'850.51};
+        pos = binance::futures::long_position{trade{amount_t{value_of(open)*0.17046}, open, ptime()}, leverage};
+        open = price_t{6'629.53};
+        pos.add_open(trade{amount_t{value_of(open)*0.17046}, open, ptime()});
+        open = price_t{6'261.22};
+        pos.add_open(trade{amount_t{value_of(open)*0.34092}, open, ptime()});
+        open = price_t{5'892.91};
+        pos.add_open(trade{amount_t{value_of(open)*0.68185}, open, ptime()});
+        pos.add_close(trade{pos.size(), price_t{6'074.48}, ptime()});
+        BOOST_REQUIRE_CLOSE(value_of(pos.total_realized_profit<amount_t>()), -166.85, 1);
+
+        open = price_t{45'582.47};
+        pos = binance::futures::long_position{trade{amount_t{value_of(open)*0.03307}, open, ptime()}, leverage};
+        open = price_t{44'112.07};
+        pos.add_open(trade{amount_t{value_of(open)*0.03307}, open, ptime()});
+        pos.add_close(trade{pos.size(), price_t{46'656.07}, ptime()});
+        BOOST_REQUIRE_CLOSE(value_of(pos.total_realized_profit<amount_t>()), 119.7, 1);
+    }
+
+    BOOST_AUTO_TEST_CASE(leverage_test)
+    {
+        std::size_t leverage{10};
+        price_t open{7'176.47};
+        binance::futures::long_position pos{
+                trade{amount_t{value_of(open)*(1.74815/static_cast<double>(leverage))}, open, ptime()}, leverage};
+        pos.add_close(trade{pos.size(), price_t{7'242.66}, ptime()});
+        BOOST_REQUIRE_CLOSE(value_of(pos.total_realized_profit<amount_t>()), 115.28, 1);
+
+        open = price_t{9'748.24};
+        pos = binance::futures::long_position{
+                trade{amount_t{value_of(open)*(1.33945/static_cast<double>(leverage))}, open, ptime()}, leverage};
+        open = price_t{9'443.61};
+        pos.add_open(trade{amount_t{value_of(open)*(1.33945/static_cast<double>(leverage))}, open, ptime()});
+        pos.add_close(trade{pos.size(), price_t{9'627.19}, ptime()});
+        BOOST_REQUIRE_CLOSE(value_of(pos.total_realized_profit<amount_t>()), 83.75, 1);
+
+        open = price_t{7'099.12};
+        pos = binance::futures::long_position{
+                trade{amount_t{value_of(open)*(2.04744/static_cast<double>(leverage))}, open, ptime()}, leverage};
+        open = price_t{6'877.28};
+        pos.add_open(trade{amount_t{value_of(open)*(2.04744/static_cast<double>(leverage))}, open, ptime()});
+        open = price_t{6'655.43};
+        pos.add_open(trade{amount_t{value_of(open)*(4.09488/static_cast<double>(leverage))}, open, ptime()});
+        open = price_t{6'285.68};
+        pos.add_open(trade{amount_t{value_of(open)*(8.18976/static_cast<double>(leverage))}, open, ptime()});
+        pos.add_close(trade{pos.size(), price_t{6'038.32}, ptime()});
+        BOOST_REQUIRE_CLOSE(value_of(pos.total_realized_profit<amount_t>()), -8'447.039999999999, 1);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
