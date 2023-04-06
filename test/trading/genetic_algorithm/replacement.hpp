71f77fbed2fcaab67c69acaa18e9c3af900608b1
TomasPetricek <45512445+tomaspetricek@users.noreply.github.com>
2023-04-06 13:52:35 +0200

    Describe CSV reader and writer testing

---
+++ test/trading/genetic_algorithm/replacement.hpp
@@ -0,0 +1,20 @@
+//
+// Created by Tomáš Petříček on 05.04.2023.
+//
+
+#ifndef BACKTESTING_TEST_GENETIC_ALGORITHM_REPLACEMENT_HPP
+#define BACKTESTING_TEST_GENETIC_ALGORITHM_REPLACEMENT_HPP
+
+#include <boost/test/unit_test.hpp>
+#include <vector>
+#include <trading/genetic_algorithm/replacement.hpp>
+
+BOOST_AUTO_TEST_SUITE(genetic_algorithm_en_block_replacement_test)
+    BOOST_AUTO_TEST_CASE(usage_test)
+    {
+        trading::genetic_algorithm::en_block_replacement replacement{};
+
+    }
+BOOST_AUTO_TEST_SUITE_END()
+
+#endif //BACKTESTING_TEST_GENETIC_ALGORITHM_REPLACEMENT_HPP
